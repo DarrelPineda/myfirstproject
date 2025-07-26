f189cd2ba880ee0b93dd05d49512970767673b42
DarrelPineda <darrelpineda13@gmail.com>
2025-07-27 02:04:48 +0800

    Fix GitHub Pages showing default Flutter README

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3882352650"
+    serviceWorkerVersion: "2643479758"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "fa23aec149b6362f58ebfd6b4116d184
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "0b51556ea21ea908fd3bccc175935aba",
+"flutter_bootstrap.js": "928150e0795d861ae0b0c9c6e758b628",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
